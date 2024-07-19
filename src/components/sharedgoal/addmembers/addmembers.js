92b8667bfee9e50a59920574a30e757da049cdc6
vasanthkalli <vasanth.kalli@gmail.com>
2024-07-19 21:03:27 +0530

    adding contribution feature

--- src/components/sharedgoal/addmembers/addmembers.js
+++ src/components/sharedgoal/addmembers/addmembers.js
@@ -1,9 +1,29 @@
+import { TextField } from "@mui/material"
+import { useState } from "react"
 import { useSelector } from "react-redux"
+import Button from "@mui/material/Button"
+import {Grid} from "@mui/material"
 export default function AddMember(){
 
     const goalCatSelected  = useSelector(state => state.sharedGoalReducer.selectedGoalCategory)
     const goalName  = useSelector(state => state.sharedGoalReducer.goalName)
     const targetAmount  = useSelector(state => state.sharedGoalReducer.targetAmount)
+
+    const [membername, setmembername] = useState('')
+    const [email, setemail] = useState('')
+    const [mobilenumber, setmobilenumber] = useState()
+
+    const handleNameChange = (e) => {
+         setmembername(e.target.value)
+    }
+
+    const handleEmailCahnge = (e) => {
+        setemail(e.target.value)
+   }
+   
+   const handlePhonenumChange = (e) => {
+    setmobilenumber(e.target.value)
+}
 
     return (<div>
         Add member step
@@ -11,5 +31,20 @@ export default function AddMember(){
         Goal name is {goalName}
 
         goal targetAmount is {targetAmount}
+
+        <Grid container>
+            <Grid item>
+                <TextField value={membername} onChange = {handleNameChange} label='name' required></TextField>
+            </Grid>
+            <Grid item>
+                <TextField value={email} label='email' onChange={handleEmailCahnge} type="email" required></TextField>
+            </Grid>
+            <Grid item>
+                <TextField value={mobilenumber} label='mobile number' onChange={handlePhonenumChange} required></TextField>
+            </Grid>
+            <Grid item>
+                  <Button varain='outlined'>Add Member</Button>
+            </Grid>
+        </Grid>
     </div>)
 }
--- src/components/sharedgoal/goaldetails/goaldetails.js
+++ src/components/sharedgoal/goaldetails/goaldetails.js
@@ -1,17 +1,36 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Grid, TextField } from "@mui/material";
-import { useState } from "react";
+import { Grid, Slider, TextField, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import { setGoalName, setTargetAmount } from "../sharedgoalSlice";
+import { Button, Switch, FormControlLabel } from "@mui/material";
 
-export default function GoalDetails(){
+
+
+export default function GoalDetails() {
 
     const dispatch = useDispatch();
 
-    const goalSelected  = useSelector(state => state.sharedGoalReducer.selectedGoalCategory)
+    const goalSelected = useSelector(state => state.sharedGoalReducer.selectedGoalCategory)
 
     const [goalName, setGoalNameState] = useState('')
 
-    const [targetAmount, setTargetAmountState] = useState(0)
+    const [targetAmount, setTargetAmountState] = useState()
+
+    const [proceed, setProceed] = useState(false)
+
+    const [selecedMonths, setSelectedMonths] = useState(12)
+
+    const [interestRate, setInterestrate] = useState(7)
+
+    const [switchMonthlyContri, setswitchMonthlyContri] = useState(true)
+
+    const [monthlyContribution, setMonthyContribution] = useState()
+
+    const [amountToInvest, setAmountToInvest] = useState()
+
+    useEffect(()=> {
+        caalculateMonthyContribution();
+    }, [selecedMonths])
 
     const handlegoalName = (e) => {
         setGoalNameState(e.target.value)
@@ -19,28 +38,116 @@ export default function GoalDetails(){
     }
 
     const handleTargetAmount = (e) => {
-        setTargetAmountState(e.target.value)
-        dispatch(setTargetAmount(e.target.value))
+        setTargetAmountState(Number(e.target.value))
+        dispatch(setTargetAmount(Number(e.target.value)))
     }
 
-    const handlesendingNameToStore = () =>  {
-      
+    const handlesendingNameToStore = () => {
+
+    }
+
+    const resetGoal = () => {
+        setGoalNameState('');
+        setTargetAmountState();
+        setProceed(false);
+    }
+
+
+    const setLooksGood = () => {
+        setProceed(true)
+    }
+
+    const valueText = (value) => {
+        setSelectedMonths(value)
+        setInterestrate(value > 15 ? 7.2 : 7)
+    }
+
+
+    const handleSwitchMonthly = (event) => {
+        setswitchMonthlyContri(event.target.checked)
+
+    }
+
+    const calculateInterest = () => {
+        let i = (targetAmount * selecedMonths * interestRate)/100;
+        console.log('interest amount'+ i)
+        return i;
+    }
+
+    const calculateAmountTobeInvested = () => {
+        let interest = calculateInterest();
+        let amountToInvest =  targetAmount - interest;
+        setAmountToInvest(amountToInvest)
+        console.log('amount tobe invested'+ amountToInvest)
+        return amountToInvest;
+    }
+
+    const caalculateMonthyContribution = () => {
+        let totalAmountTobeInevsted = calculateAmountTobeInvested();
+        let monthyContributionAmount = totalAmountTobeInevsted/selecedMonths;
+        console.log('Monthly contribution'+ monthlyContribution)
+        setMonthyContribution(monthyContributionAmount)
     }
 
     return (
         <div>
             Selected Goal is {goalSelected}
-            <Grid container>
-
+            <Grid container id="top-container">
                 <Grid item>
+                    <Grid container>
+                        <Grid item>
+                            <TextField value={goalName} label='Goal name' onChange={handlegoalName} onBlur={handlesendingNameToStore} required></TextField>
 
-                    <TextField value={goalName} label='Goal name' onChange={handlegoalName} onBlur={handlesendingNameToStore}></TextField>
+                        </Grid>
 
+                        <Grid item>
+                            <TextField value={targetAmount} label='Target Amount' onChange={handleTargetAmount} required></TextField>
+                        </Grid>
+
+                        <Grid item>
+                            <Button onClick={resetGoal}>Cancel</Button>
+                        </Grid>
+                        <Grid item>
+                            <Button onClick={setLooksGood}>Proceed</Button>
+                        </Grid>
+                    </Grid>
                 </Grid>
+                {proceed ? (<Grid item>
+                    <Grid container>
+                        <Grid item>
+                            <Typography variant="h6">You invest { }</Typography>
+                            <Typography variant="h6">You Get { }</Typography>
+                        </Grid>
+                        <Grid item>
+                            <Typography>Select the number of months you need to fullfill your goal</Typography>
+                            <Slider defaultValue={12} aria-label="Default" valueLabelDisplay="on"
+                                getAriaValueText={valueText}
+                                min={6} max={60}
+                            />
+                            <div>Selected Months are {selecedMonths}</div>
+                            <div>You will get interest rate of {interestRate}</div>
+                            <div>Want to contribute monthly ?
+                                <FormControlLabel control={<Switch checked={switchMonthlyContri} onChange={handleSwitchMonthly} />} label="monthly contribution" />
+                            </div>
+                        </Grid>
+                        {
+                            switchMonthlyContri ?
+                                (
 
-                <Grid item>
-                   <TextField value={targetAmount} label='Target Amount' onChange={handleTargetAmount}></TextField>
-                </Grid>
+                                    <Grid item>
+                                          <Typography>Your Monthly Contribution amount will be {monthlyContribution}</Typography>
+                                          {console.log('monthly contribution'+monthlyContribution)}
+                                    </Grid>
+
+                                ) :
+
+                                null
+                        }
+
+                    </Grid>
+                  
+                </Grid>) : null
+                }
             </Grid>
         </div>
     );
